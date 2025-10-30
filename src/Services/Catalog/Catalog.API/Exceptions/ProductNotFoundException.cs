19a0906a39e12d66a30f1ae9c379b2ae0eac2982
Igor Souza <igorrafael.jobs@gmail.com>
2025-10-30 10:46:22 -0300

    feat: adding getproductbyid

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException()
+            : base("The requested product was not found.")
+        {
+        }
+    }
+}
