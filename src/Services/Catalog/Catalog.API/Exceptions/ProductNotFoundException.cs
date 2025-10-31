ed8a4e7794dcd23034a879e9b250e37ab76a2f16
Igor Souza <igorrafael.jobs@gmail.com>
2025-10-31 13:21:40 -0300

    feat: adding exceptions

--- src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -1,9 +1,11 @@
-﻿namespace Catalog.API.Exceptions
+﻿using BuildingBlocks.Exceptions;
+
+namespace Catalog.API.Exceptions
 {
-    public class ProductNotFoundException : Exception
+    public class ProductNotFoundException : NotFoundException
     {
-        public ProductNotFoundException()
-            : base("The requested product was not found.")
+        public ProductNotFoundException(Guid Id)
+            : base("Product", Id)
         {
         }
     }
