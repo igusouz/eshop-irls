d02c2cda4e62f455964d5d8e39f6cd13297f7753
Igor Souza <igorrafael.jobs@gmail.com>
2025-10-31 12:03:13 -0300

    feat: adding valitadors to update and delete functions.

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -6,6 +6,30 @@ namespace Catalog.API.Products.UpdateProduct
 
     public record UpdateProductResult(bool IsSuccess);
 
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(command => command.Id)
+                .NotEmpty()
+                .WithMessage("Product Id is required");
+
+            RuleFor(command => command.Name)
+                .NotEmpty()
+                .WithMessage("Name is required")
+                .Length(2, 150)
+                .WithMessage("Product name must be between 2 and 150 characters");
+
+            RuleFor(command => command.Category)
+                .NotEmpty()
+                .WithMessage("Category is required");
+
+            RuleFor(command => command.Price)
+                .GreaterThan(0)
+                .WithMessage("Price must be greater than 0");
+        }
+    }
+
     internal class UpdateProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
