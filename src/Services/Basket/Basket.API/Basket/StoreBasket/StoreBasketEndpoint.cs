0f8e5c928ba92bd2470cf8dbba4a9930b00147f6
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-16 21:46:23 -0300

    feat: adding storebasket endpoit and handler

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,6 +1,31 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+using Mapster;
+
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketEndpoint
+    public record StoreBasketRequest(ShoppingCart Cart);
+
+    public record StoreBasketResponse(string UserName);
+
+    public class StoreBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<StoreBasketResponse>();
+
+                return Results.Created($"/basket/{response.UserName}", response);
+            })
+            .WithName("CreateBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Creates a new shopping basket for a user")
+            .WithDescription("Creates a new shopping basket for a user with the provided shopping cart details.");
+        }
     }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,28 @@
 ﻿namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator< StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Shopping cart must be provided.");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("User name must be provided.");
+        }
+    }
+
+    public class StoreBasketCommmandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand commmand, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = commmand.Cart;
+
+            // TODO: Implement the logic to store the shopping cart
+            // TODO: Update cache
+            
+            return new StoreBasketResult("swn");
+        }
     }
 }
