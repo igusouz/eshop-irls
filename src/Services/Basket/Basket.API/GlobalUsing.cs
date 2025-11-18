1c7f92d2fa05274ca13de528e3e8fcc8cdc3064a
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-18 15:02:30 -0300

    feat: adding healtch check to basketapi

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -10,3 +10,5 @@ global using BuildingBlocks.Exceptions;
 global using Basket.API.Execptions;
 global using Basket.API.Data;
 global using BuildingBlocks.Exceptions.Handler;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
