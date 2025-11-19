45210551ac9b23b8e47f3f442a3671d7aaceba47
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-19 00:30:24 -0300

    feat: Adding get discount method

--- src/Services/Discount/Discount.Grpc/GlobalUsing.cs
+++ src/Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -2,3 +2,5 @@
 global using Discount.Grpc.Models;
 global using Microsoft.EntityFrameworkCore;
 global using Discount.Grpc.Data;
+global using Mapster;
+global using Grpc.Core;
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,22 @@
+﻿namespace Discount.Grpc.Services
+{
+    public class DiscountService 
+        (DiscountContext dbContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext
+                .Coupons
+                .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+            if (coupon is null)
+                coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+    }
+}
