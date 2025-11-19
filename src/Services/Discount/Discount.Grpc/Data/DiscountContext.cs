c8c81baa424467e25d2c3f4633e4716cf8b94a3d
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-18 23:45:20 -0300

    feat: configuring data folder and ef connectionstring

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
