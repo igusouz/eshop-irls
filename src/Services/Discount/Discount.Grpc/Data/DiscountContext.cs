67698ee0e41d87d5da584b073c3535384391d42a
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-19 00:02:18 -0300

    feat: adding discountcontext to db and creating migrations

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -8,5 +8,13 @@
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+                );
+        }
     }
 }
