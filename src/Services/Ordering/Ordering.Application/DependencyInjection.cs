a9b63513bfae23fbd05830855608e781a6382078
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-24 12:43:48 -0300

    feat: adding dependency injection to ordering api

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // servives.AddMediatR(cfg =>
+            // {    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            // });
+
+            return services;
+        }
+    }
+}
