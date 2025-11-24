1540ccc8e72a07da01573b18c92f3400accb0414
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-24 14:45:37 -0300

    feat: adding strongly typed ids in dd

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,12 +1,12 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class Order : Aggregate<Guid>
+    public class Order : Aggregate<OrderId>
     {
         private readonly List<OrderItem> _orderItems = new();
         private IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
 
-        public Guid CustomerId { get; private set; } = default!;
-        public string OrderName { get; set; } = default!;
+        public CustomerId CustomerId { get; private set; } = default!;
+        public OrderName OrderName { get; set; } = default!;
         public Address ShippingAddress { get; private set; } = default!;
         public Address BillingAddress { get; private set; } = default!;
         public Payment Payment { get; private set; } = default!;
