d6d6e8f20fc9bea973ba38f768ad517ef8dfcc04
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-26 11:08:21 -0300

    feat: adding dispatchdomainevents to ordering api

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application
 {
@@ -6,9 +7,10 @@ namespace Ordering.Application
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            // servives.AddMediatR(cfg =>
-            // {    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-            // });
+            services.AddMediatR(cfg =>
+            {    
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
 
             return services;
         }
