7e0884e2a8ec9646a10f54ef4718b82e1978de46
Igor Souza <igorrafael.jobs@gmail.com>
2025-11-26 20:50:16 -0300

    feat: implementing interface app db context in infra layer

--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,9 @@
 ﻿using System.Reflection;
-using Microsoft.EntityFrameworkCore;
-using Ordering.Domain.Models;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure.Data
 {
-    internal class ApplicationDbContext : DbContext
+    internal class ApplicationDbContext : DbContext, IApplicationDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
