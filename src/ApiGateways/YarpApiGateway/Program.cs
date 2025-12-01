e7542e46f568ec386de8d6eb13ccb29a5b079155
Igor Souza <igorrafael.jobs@gmail.com>
2025-12-01 13:05:41 -0300

    feat: Integrate YARP reverse proxy into the application

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 // Configure HTTP request pipeline
+app.MapReverseProxy();
 
 app.Run();
